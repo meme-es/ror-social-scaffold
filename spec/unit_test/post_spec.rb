8eeb4c0dcb709d1daaa38215a000839c663d7f9b
bertil291utn <bertilt4@gmail.com>
2020-05-30 11:35:24 -0500

    Add with_long_message attribute to content field

--- spec/unit_test/post_spec.rb
+++ spec/unit_test/post_spec.rb
@@ -3,6 +3,7 @@ require_relative '../rails_helper'
 RSpec.describe Post, type: :model do
   
   describe 'validations checks' do
+    it { should validate_length_of(:content).is_at_most(1000).with_long_message('1000 characters in post is the maximum allowed.') }
     it { is_expected.to validate_presence_of(:content) }
   end
 
