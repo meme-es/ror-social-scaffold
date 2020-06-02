227c3cabfa7431c2a2034f7c7201cca7ca2413e4
bertil291utn <bertilt4@gmail.com>
2020-06-02 15:25:35 -0500

    Two rows for friendship confirmation - After accept an invitation add a second row with the same friendship information

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,18 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = Friendship.new(user_id: current_user.id, friend_id: params[:friend_id], confirmed: false)
-    @friendship.save
+    friendship = Friendship.new(user_id: current_user.id, friend_id: params[:friend_id], confirmed: false)
+    friendship.save
 
     redirect_to users_url, notice: 'Friend request sent'
   end
 
   def confirm
-    friendship = Friendship.where(user_id: params[:friend_id], friend_id: current_user.id)
-    friendship.update(confirmed: true)
+    friendship1 = Friendship.new(user_id: current_user.id , friend_id:  params[:friend_id], confirmed: true)
+    friendship1.save
+
+    friendship2 = Friendship.where(user_id: params[:friend_id], friend_id: current_user.id)
+    friendship2.update(confirmed: true)
+
 
     redirect_to users_url, notice: 'Friend request accepted'
   end
